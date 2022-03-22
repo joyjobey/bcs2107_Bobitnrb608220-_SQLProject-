a965ffe29e89183085db726df4f40e68dc7dde06
joyjobey <101697846+joyjobey@users.noreply.github.com>
2022-03-22 14:13:18 +0300

    Add files via upload

--- management.sql
+++ management.sql
@@ -1,4 +1,25 @@
 CREATE DATABASE management;
+-- drop database management
+use management;
+
+CREATE TABLE `manager` (
+  `staffid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`staffid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `clients` (
+  `name` varchar(20) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `rentdate` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `trailerpark` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Address` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `trailer` (
   `ID` int NOT NULL AUTO_INCREMENT,
@@ -16,13 +37,6 @@ CREATE TABLE `trailer` (
   CONSTRAINT `trailer_ibfk_3` FOREIGN KEY (`client`) REFERENCES `clients` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `clients` (
-  `name` varchar(20) DEFAULT NULL,
-  `id` int NOT NULL AUTO_INCREMENT,
-  `rentdate` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 CREATE TABLE `flats` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `address` varchar(20) DEFAULT NULL,
@@ -36,18 +50,6 @@ CREATE TABLE `flats` (
   CONSTRAINT `flats_ibfk_2` FOREIGN KEY (`client`) REFERENCES `clients` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `manager` (
-  `staffid` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`staffid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `trailerpark` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `Address` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 insert into manager( 
 name) values ( 'Humphy');
 
@@ -59,23 +61,23 @@ values
 ('Jack','2022-01-23'),
 ('Peter','2022-04-20');
 
-insert into trailer(trailernumber, trailerpark, manager,client, rentcharge) values
-(10, 1,1,1,1500),
-(10, 1,1,2,1500),
-(10, 1,1,8,1500),
-(10, 1,1,9,1500),
-(10, 1,1,10,1500);
-
 insert into trailerpark(Address) values ('avenue 12'),
 ('Kitengela'),('South B');
 
+insert into trailer(trailernumber, trailerpark, manager,client, rentcharge) values
+(10, 5,2,13,1500),
+(10, 6,2,14,1500),
+(10, 7,2,15,1500),
+(10, 5,2,16,1500),
+(10, 6,2,17,1500);
+
 insert into flats(address,manager,client, rentcharge)
 values
-('Karen', 1, 1, 2000),
-('Langata', 1, 2, 2000),
-('Nakuru', 1, 8, 2000),
-('Tigoni', 1, 9, 2000),
-('Karen', 1, 10, 2000);
+('Karen', 2, 13, 2000),
+('Langata', 2, 14, 2000),
+('Nakuru', 2, 15, 2000),
+('Tigoni', 2, 16, 2000),
+('Karen', 2, 17, 2000);
 
 select * from clients;
 select * from flats;
